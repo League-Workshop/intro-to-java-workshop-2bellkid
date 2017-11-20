0fe0d86ed227576f47227e551f4be2dfaf4d67cb
100297 <league@WTS7.attlocal.net>
2017-11-20 13:47:28 -0800

    day2workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	System.out.println("Hello world!");
+		Robot rusty = new Robot("mini");
+		rusty.sparkle();
+	    rusty.setWindowColor(60, 229, 235 );
+	    rusty.penDown();
+	    rusty.setPenColor(176, 60, 235);
+	    rusty.move(100);
+	    rusty.turn(90);
+	    rusty.move(100);
+	    rusty.turn(90);
+	    rusty.move(100);
+	    rusty.turn(90);
+	    rusty.move(100);
 	}
 }
